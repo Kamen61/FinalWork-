1a588498672f5ef450ac18857d1789fa3fea3e2f
Kamen <canen321@gmail.com>
2022-08-05 18:16:02 +0300

    created a new array with words and output to the console

--- Program.cs
+++ Program.cs
@@ -2,9 +2,24 @@
 char[] separators = { ',', ';' };
 int outputArraySize = 0;
 int longElementCondition = 3;
-string[]  enteredString = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+string[] enteredString = Console.ReadLine()!.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 for (int i = 0; i < enteredString.Length; i++)
 {
     if (enteredString[i].Length <= longElementCondition)
         outputArraySize++;
 }
+string[] result = new string[outputArraySize];
+if (outputArraySize != 0)
+
+{
+    int indexCounter = 0;
+    for (int i = 0; i < enteredString.Length; i++)
+    {
+        if (enteredString[i].Length <= longElementCondition)
+        {
+            result[indexCounter] = enteredString[i];
+            indexCounter++;
+        }
+    }
+}
+System.Console.WriteLine($"[{string.Join(" , ", result)}]");
