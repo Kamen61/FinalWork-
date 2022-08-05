1a297a57fa4b2174c12e80efec7bc900021fcb6c
Kamen <canen321@gmail.com>
2022-08-05 18:13:09 +0300

    Count how many words fit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите несколько слов или цифр через запятую : ");
+char[] separators = { ',', ';' };
+int outputArraySize = 0;
+int longElementCondition = 3;
+string[]  enteredString = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+for (int i = 0; i < enteredString.Length; i++)
+{
+    if (enteredString[i].Length <= longElementCondition)
+        outputArraySize++;
+}
